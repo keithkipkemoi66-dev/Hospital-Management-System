501707a74dbf4035caa3e354000f41c6d6a156fa
keith mebur <keithkipkemoi66@gmail.com>
2026-03-20 17:37:20 +0300

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,20 +1,20 @@
 package HOSPITALMANAGEMENTSYSTEM;
 
-public class Patient extends HospitalStaff implements Evaluatable {
-    private double gpa;
+public class Patient extends Person implements Billable {
+    private String ailment;
 
-    public Patient(String name, int id, double gpa) {
-        super(name, id);
-        this.gpa = gpa;
+    public Patient(String name, int id, String ailment) {
+        super(name, id); 
+        this.ailment = ailment;
     }
 
     @Override
-    public void displayRole() {
-        System.out.println("[Student] Name: " + getName() + " | GPA: " + gpa);
+    public void displayInfo() {
+        System.out.println("[Patient] " + getName() + " (ID: " + getId() + ") | Ailment: " + ailment);
     }
 
     @Override
-    public void evaluatePerformance() {
-        System.out.println("Evaluating student " + getName() + " based on Semester GPA.");
+    public void generateInvoice() {
+        System.out.println(">>> Generating medical bill for " + getName() + " (Treatment: " + ailment + ")");
     }
 }
